80a0f7c947de162af34794c14118d0d3992760a2
Md Rayhan Ali <78335557+rayhan2001@users.noreply.github.com>
2023-03-03 00:14:50 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $faker = Faker::create();
+        foreach (range(1,500) as $key =>$value){
+            DB::table('blogs')
+                ->insert([
+                    'title'=>$faker->text,
+                    'name'=>$faker->text,
+                    'description'=>$faker->text,
+                ]);
+        }
+    }
+}
